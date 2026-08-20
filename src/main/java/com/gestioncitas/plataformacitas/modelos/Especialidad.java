df349c315ebab2015767c20f9119581d01812e7c
unice786 <maycol.menendez.84@gmail.com>
2026-08-20 00:31:58 -0600

    fix: restaurar entidad Especialidad completa en merge SCRUM-14

--- src/main/java/com/gestioncitas/plataformacitas/modelos/Especialidad.java
+++ src/main/java/com/gestioncitas/plataformacitas/modelos/Especialidad.java
@@ -1,4 +1,36 @@
 package com.gestioncitas.plataformacitas.modelos;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "especialidades")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Especialidad {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String nombre;
+
+	@Column(length = 255)
+	private String descripcion;
+
+	@OneToMany(mappedBy = "especialidad")
+	private List<Empleado> empleados = new ArrayList<>();
 }
